b66a0b8688c964e0a9a4773501bf7f4693425b47
Kazungu1 <kazunguderriq@gmail.com>
2022-08-16 11:45:30 +0300

    add methods

--- methods.rb
+++ methods.rb
@@ -1,4 +1,33 @@
 # Your code here!
 def greet_programmer
-    puts "Hello, programmer!"
+  puts "Hello, programmer!"
 end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+greet("Jimmy")
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+greet_with_default
+
+def add(num1, num2)
+  num1 + num2
+end
+
+add(1, 1)
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+
+    (number)/2
+end
+halve("sum")
